34e672d259f48b2b53293ceae44cb7a3669aa7a3
Ice Maya <echaymaya@gmail.com>
2018-11-16 12:54:33 +1300

    updated table with languages coloumn

--- migrations/20181116100428_vegeta.js
+++ migrations/20181116100428_vegeta.js
@@ -1,12 +1,17 @@
-exports.up = (knex, Promise) => {
+exports.up = (knex) => {
   return knex.schema.createTable('vegeta', (table) => {
     table.increments('id').primary()
     table.string('command')
     table.string('dogName')
-    table.string('language')
+    table.string('russian')
+    table.string('maori')
+    table.string('bahasa')
+    table.string('korean')
+    table.string('samoan')
+    table.string('dutch')
   })
 }
 
-exports.down = (knex, Promise) => {
+exports.down = (knex) => {
   return knex.schema.dropTable('vegeta')
 }
